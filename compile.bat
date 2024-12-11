b47c9d8c6d24d4021de64fdbb1a1c4605a362afd
571 <qiyingwu571@gmail.com>
2024-12-11 20:16:45 +0800

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 @echo off
-cd C:\Users\Arrie\Desktop\thesis-main
+cd C:\Users\r10341027\Desktop\thesis-main
 .\win-bash\PortableGit\bin\bash.exe win-build-html.sh
 .\win-bash\PortableGit\bin\bash.exe win-build-tex.sh
